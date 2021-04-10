62779c6597486b671ccfea10f1756f23d15a361e
Arthur S <arstrini@gmail.com>
2021-04-09 22:38:25 -0300

    Back to routes

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,21 @@
-#![deny(warnings)]
-
-use futures::{FutureExt, StreamExt};
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
+    // GET /
+    let hello_world = warp::fs::dir("/home/sua/git/suamai/raspwarp/public");
 
-    let routes = warp::path("echo")
-        // The `ws()` filter will prepare the Websocket handshake.
-        .and(warp::ws())
-        .map(|ws: warp::ws::Ws| {
-            // And then our closure will be called when it completes...
-            ws.on_upgrade(|websocket| {
-                // Just echo all messages back...
-                let (tx, rx) = websocket.split();
-                rx.forward(tx).map(|result| {
-                    if let Err(e) = result {
-                        eprintln!("websocket error: {:?}", e);
-                    }
-                })
-            })
-        });
+    let sum = warp::path!("sum" / u32 / u32).map(|a, b| format!("{} + {} = {}", a, b, a + b));
+
+    let times =
+        warp::path!(u16 / "times" / u16).map(|a, b| format!("{} times {} = {}", a, b, a * b));
+
+    let help = warp::path::end()
+        .map(|| "This is the Math API. Try calling /math/sum/:u32/:u32 or /math/:u16/times/:u16");
+
+    let math = warp::path("math").and(help.or(sum).or(times));
+
+    let routes = warp::get().and(hello_world.or(math));
 
     warp::serve(routes).run(([127, 0, 0, 1], 80)).await;
 }
